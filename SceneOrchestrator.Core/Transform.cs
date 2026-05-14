8daf59bcc42240a6168450c41f9b1b5076315551
arzacape <e.arzak04@gmail.com>
2026-05-14 19:06:15 +0200

    initial transform class code implementing serialization

--- SceneOrchestrator.Core/Transform.cs
+++ SceneOrchestrator.Core/Transform.cs
@@ -1,8 +1,51 @@
 using System.Numerics;
+using System.Text.Json;
+using System.Text.Json.Serialization;
 
-class Transform
+namespace SceneOrchestrator.Core;
+
+public class Vector3Converter : JsonConverter<Vector3>
 {
-    public Matrix4x4 LocalMatrix { get; private set; }
-    public Vector4 Position => LocalMatrix.W;
+    public override Vector3 Read(ref Utf8JsonReader reader, Type typeToConvert, JsonSerializerOptions options)
+    {
+        return new Vector3(reader.GetSingle(), reader.GetSingle(), reader.GetSingle());
+    }
 
+
+    public override void Write(Utf8JsonWriter writer, Vector3 value, JsonSerializerOptions options)
+    {
+        writer.WriteStartObject();
+        writer.WriteNumber("x", value.X);
+        writer.WriteNumber("y", value.Y);
+        writer.WriteNumber("z", value.Z);
+        writer.WriteEndObject();
+    }
+}
+
+public class QuaternionConverter : JsonConverter<Quaternion>
+{
+    public override Quaternion Read(ref Utf8JsonReader reader, Type typeToConvert, JsonSerializerOptions options)
+    {
+        return new Quaternion(reader.GetSingle(), reader.GetSingle(), reader.GetSingle(), reader.GetSingle());
+    }
+
+    public override void Write(Utf8JsonWriter writer, Quaternion value, JsonSerializerOptions options)
+    {
+        writer.WriteStartObject();
+        writer.WriteNumber("x", value.X);
+        writer.WriteNumber("y", value.Y);
+        writer.WriteNumber("z", value.Z);
+        writer.WriteNumber("w", value.W);
+        writer.WriteEndObject();
+    }
+}
+
+public class Transform
+{
+    [JsonConverter(typeof(Vector3Converter))]
+    public Vector3 Position { get; set; } = Vector3.Zero;
+    [JsonConverter(typeof(QuaternionConverter))]
+    public Quaternion Rotation { get; set; } = Quaternion.Identity;
+    [JsonConverter(typeof(Vector3Converter))]
+    public Vector3 Scale { get; set; } = Vector3.One;
 }
