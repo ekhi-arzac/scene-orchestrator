34376154945513528a0e711786da2f34d01fdb09
arzacape <e.arzak04@gmail.com>
2026-05-23 16:47:08 +0200

    added mesh class

---
+++ SceneOrchestrator.Core/Mesh.cs
@@ -0,0 +1,15 @@
+using System.Numerics;
+
+namespace SceneOrchestrator.Core;
+
+public interface IMesh
+{
+    string MeshPath { get; set; }
+}
+
+public class MeshNode(string tag, Vector3 position, Quaternion rotation, string meshPath)
+    : SceneNode(tag, position, rotation),
+        IMesh
+{
+    public string MeshPath { get; set; } = meshPath;
+}
