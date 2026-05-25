65ed4570930b9c9a7f9b74181684be186b6854b9
arzacape <e.arzak04@gmail.com>
2026-05-25 20:01:23 +0200

    simplified camera class

--- SceneOrchestrator.Core/Camera.cs
+++ SceneOrchestrator.Core/Camera.cs
@@ -2,11 +2,18 @@ using System.Numerics;
 
 namespace SceneOrchestrator.Core;
 
+public enum CameraType
+{
+    Perspective,
+    Orthographic,
+}
+
 /// <summary>
 /// Represents an interface for a camera that projects objects in a 3D scene using a projection matrix.
 /// </summary>
 interface ICamera
 {
+    public CameraType Type { get; }
     public Matrix4x4 ViewMatrix();
     public Matrix4x4 ProjectionMatrix { get; }
 
@@ -17,80 +24,29 @@ interface ICamera
 }
 
 /// <summary>
-/// Represents an abstract camera that projects objects in a 3D scene using a projection matrix.
+/// Represents a camera that projects objects in a 3D scene using a projection matrix.
 /// </summary>
-public abstract class Camera(
+public class Camera(
     string tag,
     Vector3 position,
     Quaternion rotation,
+    CameraType type,
     float fieldOfView,
     float nearPlane,
     float farPlane,
     float aspectRatio
 ) : SceneNode(tag, position, rotation), ICamera
 {
+    public CameraType Type { get; set; } = type;
+
     public Matrix4x4 ViewMatrix() => Transform.ViewMatrix();
 
-    public abstract Matrix4x4 ProjectionMatrix { get; protected set; }
+    public Matrix4x4 ProjectionMatrix => Type == CameraType.Perspective
+        ? Matrix4x4.CreatePerspectiveFieldOfView(FieldOfView, AspectRatio, NearPlane, FarPlane)
+        : Matrix4x4.CreateOrthographic(FieldOfView * AspectRatio, FieldOfView, NearPlane, FarPlane);
 
-    public float FieldOfView { get; protected set; } = fieldOfView;
-    public float NearPlane { get; protected set; } = nearPlane;
-    public float FarPlane { get; protected set; } = farPlane;
-    public float AspectRatio { get; protected set; } = aspectRatio;
-
-    /// <summary>
-    /// Updates the perspective projection matrix of the camera with the specified field of view, near plane, far plane, and aspect ratio.
-    ///
-    /// <exception cref="ArgumentOutOfRangeException">
-    /// Thrown when <paramref name="fov"/> is not positive, <paramref name="near"/> is not positive, or <paramref name="far"/> is not positive.
-    /// </exception>
-    /// </summary>
-    public void UpdateProjection(float fov, float near, float far, float aspect)
-    {
-        FieldOfView = fov;
-        NearPlane = near;
-        FarPlane = far;
-        AspectRatio = aspect;
-
-        ProjectionMatrix = Matrix4x4.CreatePerspective(
-            FieldOfView,
-            AspectRatio,
-            NearPlane,
-            FarPlane
-        );
-    }
-}
-
-/// <summary>
-/// Represents a perspective camera that projects objects in a 3D scene using a perspective projection.
-/// </summary>
-public class PerspectiveCamera(
-    string tag,
-    Vector3 position,
-    Quaternion rotation,
-    float fieldOfView,
-    float nearPlane,
-    float farPlane,
-    float aspectRatio
-) : Camera(tag, position, rotation, fieldOfView, nearPlane, farPlane, aspectRatio)
-{
-    public override Matrix4x4 ProjectionMatrix { get; protected set; } =
-        Matrix4x4.CreatePerspectiveFieldOfView(fieldOfView, aspectRatio, nearPlane, farPlane);
-}
-
-/// <summary>
-/// Represents an orthographic camera that projects objects in a 3D space using an orthographic projection.
-/// </summary>
-public class OrthographicCamera(
-    string tag,
-    Vector3 position,
-    Quaternion rotation,
-    float fieldOfView,
-    float nearPlane,
-    float farPlane,
-    float aspectRatio
-) : Camera(tag, position, rotation, fieldOfView, nearPlane, farPlane, aspectRatio)
-{
-    public override Matrix4x4 ProjectionMatrix { get; protected set; } =
-        Matrix4x4.CreateOrthographic(fieldOfView * aspectRatio, fieldOfView, nearPlane, farPlane);
+    public float FieldOfView { get; set; } = fieldOfView;
+    public float NearPlane { get; set; } = nearPlane;
+    public float FarPlane { get; set; } = farPlane;
+    public float AspectRatio { get; set; } = aspectRatio;
 }
