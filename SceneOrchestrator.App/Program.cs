4c8ba79bada880b0dd3d3f89f07a43f1a621b056
arzacape <e.arzak04@gmail.com>
2026-05-28 21:18:29 +0200

    add program entrypoint

---
+++ SceneOrchestrator.App/Program.cs
@@ -0,0 +1,18 @@
+﻿namespace SceneOrchestrator.App;
+
+using SceneOrchestrator.UI;
+
+/// <summary>
+/// Application entry point that launches the editor UI.
+/// </summary>
+public class Program
+{
+    /// <summary>
+    /// Application entry point.
+    /// </summary>
+    public static void Main(string[] args)
+    {
+        UserInterface ui = new();
+        ui.Run();
+    }
+}
